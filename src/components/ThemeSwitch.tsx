1d6e3b32e15a0cdb0e179abdf8bfa802c3190591
Takeru KIDO <keidy2463@gmail.com>
2023-04-02 15:39:55 +0900

    Update ThemeSwitch

--- src/components/ThemeSwitch.tsx
+++ src/components/ThemeSwitch.tsx
@@ -1,31 +1,33 @@
 import { isDarkModeState } from "@/states/color";
 import DarkModeIcon from "@mui/icons-material/DarkMode";
 import LightModeIcon from "@mui/icons-material/LightMode";
-import { Switch } from "@mui/material";
+import { Switch, Tooltip } from "@mui/material";
 import { useRecoilState } from "recoil";
 
 function ThemeSwitch() {
   const [isDarkMode, setIsDarkMode] = useRecoilState<boolean>(isDarkModeState);
 
   return (
-    <Switch
-      checked={isDarkMode}
-      onChange={() => setIsDarkMode(!isDarkMode)}
-      icon={<LightModeIcon sx={{ color: "white" }} />}
-      checkedIcon={<DarkModeIcon sx={{ color: "white" }} />}
-      sx={{
-        width: 58,
-        height: 38,
-        padding: 0,
-        "& .MuiSwitch-switchBase": {
+    <Tooltip title={isDarkMode ? "ダーク" : "ライト"}>
+      <Switch
+        checked={isDarkMode}
+        onChange={() => setIsDarkMode(!isDarkMode)}
+        icon={<LightModeIcon sx={{ color: "white" }} />}
+        checkedIcon={<DarkModeIcon sx={{ color: "white" }} />}
+        sx={{
+          width: 58,
+          height: 38,
           padding: 0,
-          margin: "7px",
-        },
-        "& .MuiSwitch-track": {
-          borderRadius: 38 / 2,
-        },
-      }}
-    />
+          "& .MuiSwitch-switchBase": {
+            padding: 0,
+            margin: "7px",
+          },
+          "& .MuiSwitch-track": {
+            borderRadius: 38 / 2,
+          },
+        }}
+      />
+    </Tooltip>
   );
 }
 
