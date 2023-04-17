361c0d1a77423aea4a0dbc8338d7ffef907b7b0e
Takeru KIDO <keidy2463@gmail.com>
2023-04-18 00:05:22 +0900

    Delete useless types

--- src/types/props.ts
+++ src/types/props.ts
@@ -1,5 +1,5 @@
 import { Dispatch, ReactNode, SetStateAction } from "react";
-import { ExplanationSentences, IncorrectChoices, Sentence } from "./backend";
+import { ExplanationSentences, Sentence } from "./backend";
 
 export type ApplyMUIProps = {
   children: ReactNode;
@@ -37,36 +37,6 @@ export type ExplanationsDialogProps = {
 export type NotTranslatedSnackbarProps = {
   open: boolean;
   onClose: () => void;
-};
-
-export type TestChoiceContentProps = {
-  choices: Sentence[];
-  isCorrectedMulti: boolean;
-  translatedChoices: string[] | undefined;
-  selectedIdxes: number[];
-  correctIdxes: number[];
-  isDisabledChoiceInput: boolean;
-  onChangeChoiceInput: (idx: number, isCorrectedMulti: boolean) => void;
-};
-
-export type TestExplanationIncorrectChoiceContentProps = {
-  choices: Sentence[];
-  translatedChoices: string[] | undefined;
-  incorrectChoices: IncorrectChoices;
-  translatedIncorrectChoices: string[] | undefined;
-};
-
-export type TestSentenceContentProps = {
-  sentences: Sentence[];
-  translatedSentences: string[] | undefined;
-};
-
-export type TestTranslationErrorContentProps = {
-  sentences: Sentence[];
-  setTranslatedSentences: React.Dispatch<
-    React.SetStateAction<string[] | undefined>
-  >;
-  setIsNotTranslatedSentences: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
 export type ThisLayoutProps = {
