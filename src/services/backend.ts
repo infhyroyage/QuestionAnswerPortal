c2ced4e18bbcd254bf1766dffefdce91634fdbc7
Takeru KIDO <keidy2463@gmail.com>
2023-04-24 00:57:56 +0900

    Refactor backend.ts

--- src/services/backend.ts
+++ src/services/backend.ts
@@ -11,34 +11,23 @@ import { Method } from "@/types/backend";
 /**
  * axiosを用いて、バックエンドのREST APIにアクセスしたレスポンスをそのまま返す
  * @param {Method} method HTTPメソッドタイプ
- * @param {string} path パス
+ * @param {string} url URL
  * @param {string | undefined} accessToken MSALで発行したアクセストークン
  * @param {D | undefined} data リクエストデータ
  * @returns {Promise<T>} レスポンス
  */
 const callByAxios = async <T, D>(
   method: Method,
-  path: string,
+  url: string,
   accessToken?: string | undefined,
   data?: D
 ): Promise<T> => {
   try {
-    const apiUri: string | undefined = process.env.NEXT_PUBLIC_API_URI;
-    if (!apiUri) {
-      throw new Error("Unset NEXT_PUBLIC_API_URI");
-    }
-    const url: string = `${apiUri}/api${path}`;
-
-    // 非localhost環境のみ、アクセストークンをヘッダーに追加
-    let headers: AxiosHeaders | undefined = undefined;
-    if (apiUri !== "http://localhost:9229") {
-      if (!accessToken) {
-        throw new Error("Unset accessToken");
-      }
-      headers = new AxiosHeaders({
-        "X-Access-Token": accessToken,
-      });
-    }
+    let headers: AxiosHeaders | undefined = accessToken
+      ? new AxiosHeaders({
+          "X-Access-Token": accessToken,
+        })
+      : undefined;
 
     // axios実行
     let res;
@@ -80,14 +69,12 @@ export const accessBackend = async <T, D = any>(
   accountInfo: AccountInfo | null,
   data?: D
 ): Promise<T> => {
-  const apiUri = process.env.NEXT_PUBLIC_API_URI;
-  if (!apiUri) {
-    throw new Error("Unset NEXT_PUBLIC_API_URI");
-  }
+  const apiUri: string | undefined = process.env.NEXT_PUBLIC_API_URI;
+  const url: string = `${apiUri}/api${path}`;
 
   // localhost環境
   if (apiUri === "http://localhost:9229") {
-    return await callByAxios<T, D>(method, path, undefined, data);
+    return await callByAxios<T, D>(method, url, undefined, data);
   }
 
   // 非localhost環境 & ログイン済
@@ -98,14 +85,14 @@ export const accessBackend = async <T, D = any>(
       scopes: backendAccessScopes.accessAsUser,
       account,
     });
-    return await callByAxios<T, D>(method, path, msalRes.accessToken, data);
+    return await callByAxios<T, D>(method, url, msalRes.accessToken, data);
   } catch (err) {
     if (err instanceof InteractionRequiredAuthError) {
       try {
         const msalRes: AuthenticationResult = await instance.acquireTokenPopup({
           scopes: backendAccessScopes.accessAsUser,
         });
-        return await callByAxios<T, D>(method, path, msalRes.accessToken, data);
+        return await callByAxios<T, D>(method, url, msalRes.accessToken, data);
       } catch (e) {
         throw e;
       }
