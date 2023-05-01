90dc6cd925b41589dc3be77c810896c821d02ec7
Takeru KIDO <keidy2463@gmail.com>
2023-05-01 17:47:13 +0900

    Delete _document.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,18 +2,25 @@ import ApplyMSAL from "@/components/ApplyMSAL";
 import ApplyMUI from "@/components/ApplyMUI";
 import TopBar from "@/components/TopBar";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { RecoilRoot } from "recoil";
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <RecoilRoot>
-      <ApplyMUI>
-        <ApplyMSAL>
-          <TopBar />
-          <Component {...pageProps} />
-        </ApplyMSAL>
-      </ApplyMUI>
-    </RecoilRoot>
+    <>
+      <Head>
+        <meta name="description" content="Question Answer Portal" />
+        <link rel="icon" href="favicon.ico" />
+      </Head>
+      <RecoilRoot>
+        <ApplyMUI>
+          <ApplyMSAL>
+            <TopBar />
+            <Component {...pageProps} />
+          </ApplyMSAL>
+        </ApplyMUI>
+      </RecoilRoot>
+    </>
   );
 }
 
